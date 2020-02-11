c67e3c1ba54320719233ecfb7ab46ef6f3fd1e58
Rahul <Rahul@LAPTOP-S7TE49N1>
2020-02-12 03:17:02 +0530

    first commit

---
+++ BootApp21-BootJdbcH2/src/main/java/com/tex/BootApp21BootJdbcH2Application.java
@@ -0,0 +1,13 @@
+package com.tex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApp21BootJdbcH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootApp21BootJdbcH2Application.class, args);
+	}
+
+}
---
+++ BootApp21-BootJdbcH2/src/test/java/com/tex/BootApp21BootJdbcH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootApp21BootJdbcH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
